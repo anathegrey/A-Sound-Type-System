7c4fbfc842c7360b49d0a7b7d868b1560f4fd018
anathegrey <anajorgecsoaresalmeida@gmail.com>
2021-02-06 23:17:32 +0000

    bela merde

--- SoundData.hs
+++ SoundData.hs
@@ -26,7 +26,7 @@ module SoundData where
                 | Seq Cmd Cmd
                 | If Expr Cmd Cmd
                 | While Expr Cmd
-                | LetVar Var Expr Cmd
+                | Letvar Expr Expr Cmd
                 deriving (Show, Eq)
 
        data Phrases = PhrasesE Expr
@@ -41,12 +41,33 @@ module SoundData where
 
        type Mem = (Address, Int) --Var or Int?
 
+       getLiteral :: Expr -> Int
+       getLiteral (Literal n) = n
+       getLiteral _ = -1
+
        isElemMem :: Address -> [Mem] -> Bool
        isElemMem _ [] = False
        isElemMem l ((u1, u2) : us) = if l == u1 then True else (isElemMem l us)       
 
-       getMemValue :: Address -> [Mem] -> Int --before entering this function, the program is supposed to check isElem
-       getMemValue l ((u1, u2) : us) = if l == u1 then u2 else (getMemValue l us)
+       getMemValue :: Address -> [Mem] -> Int
+       getMemValue l ((u1, u2) : us)
+                   | isElemMem l ((u1, u2) : us) = if l == u1 then u2 else (getMemValue l us)
 
        setMemValue :: Address -> [Mem] -> Expr -> [Mem]
-       setMemValue l ((u1, u2) : us) (Literal n) = if l == u1 then ((u1, n) : us) else (setMemValue l us (Literal n))
+       setMemValue l ((u1, u2) : us) (Literal n) = if l == u1 then ((u1, n) : us) else (u1, u2) : (setMemValue l us (Literal n))
+
+       maxMem :: [Mem] -> Int
+       maxMem ((u1, u2) : []) = u1
+       maxMem ((u1, u2) : (u3, u4) : us) = if u1 >= u3 then (maxMem ((u1, u2) : us)) else (maxMem ((u3, u4) : us))
+      
+       generateAddress :: [Mem] -> Address
+       generateAddress [] = 0
+       generateAddress u = (maxMem u) + 1
+       
+       insertLocation :: Address -> [Mem] -> [Mem]
+       insertLocation l [] = [(l, -1)]
+       insertLocation l u = (head u) : (insertLocation l (tail u)) 
+
+       except :: [Mem] -> Address -> [Mem]
+       except [] _ = []
+       except ((u1, u2) : us) l = if u1 == l then (except us l) else (u1, u2) : (except us l)
--- SoundTypeSystem.hs
+++ SoundTypeSystem.hs
@@ -1,13 +1,33 @@
 module SoundTypeSystem where
        import SoundData
 
+       substExpr :: Address -> String -> Expr -> Expr
+       substExpr l x (VarE y) = if x == y then (LocE l) else (VarE y)
+       substExpr l x (LocE a) = LocE a
+       substExpr l x (Literal n) = Literal n
+       substExpr l x (Add e1 e2) = Add (substExpr l x e1) (substExpr l x e2)
+       substExpr l x (Sub e1 e2) = Sub (substExpr l x e1) (substExpr l x e2)
+       substExpr l x (EqE e1 e2) = EqE (substExpr l x e1) (substExpr l x e2)
+       substExpr l x (Less e1 e2) = Less (substExpr l x e1) (substExpr l x e2)
+
+       substCmd :: Address -> String -> Cmd -> Cmd
+       substCmd l x (EqC e1 e2) = EqC (substExpr l x e1) (substExpr l x e2)
+       substCmd l x (Seq c1 c2) = Seq (substCmd l x c1) (substCmd l x c2)
+       substCmd l x (If expr c1 c2) = If (substExpr l x expr) (substCmd l x c1) (substCmd l x c2)
+       substCmd l x (While expr c) = While (substExpr l x expr) (substCmd l x c)
+       substCmd l x (Letvar (VarE y) expr c) = Letvar (VarE y) (substExpr l x expr) (substCmd l x c)
+
        evalExpr :: [Mem] -> Expr -> Expr
        evalExpr u (Literal n) = (Literal n)                                                                             --Base
        evalExpr u (LocE l) = if (isElemMem l u) then (Literal (getMemValue l u)) else (LocE l)                          --Contents
-       evalExpr u (Add e1 e2) = let n1 = evalExpr u e1                                                                  --Add
-                                    n2 = evalExpr u e2
-                                in Add n1 n2
-
+       evalExpr u (Add e1 e2)
+                | getLiteral n == -1 && getLiteral n1 /= -1 = n1
+                | getLiteral n /= -1 && getLiteral n1 == -1 = n
+                | getLiteral n /= -1 && getLiteral n1 /= -1 = Literal (getLiteral n + getLiteral n1)
+                | getLiteral n == -1 && getLiteral n1 == -1 = Add n n1
+                where n = evalExpr u e1
+                      n1 = evalExpr u e2
+                     
        evalCmd :: [Mem] -> Cmd -> [Mem]
        evalCmd u (EqC (LocE l) expr) = let n = evalExpr u expr                                                          --Update
                                        in if (isElemMem l u) then (setMemValue l u n) else u
@@ -23,5 +43,9 @@ module SoundTypeSystem where
                where n = evalExpr u expr
                      u1 = evalCmd u c
                      u2 = evalCmd u1 (While expr c)
-       --evalCmd u (Letvar x expr c) = let n = evalExpr u expr
-                                         
+       evalCmd u (Letvar (VarE x) expr c) = let n = evalExpr u expr
+                                                l = generateAddress u
+                                                u1 = insertLocation l u
+                                                u2 = evalCmd (evalCmd u1 (EqC (LocE l) n)) (substCmd l x c)
+                                            in except u2 l
+       evalCmd u _ = u
